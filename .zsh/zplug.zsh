1849424eab8517eab9e3e63118e315468e0212d9
Sullivan SENECHAL <soullivaneuh@gmail.com>
2019-02-19 14:43:20 +0100

    Add jq, skaffold and emoji tools :santa_claus:

--- .zsh/zplug.zsh
+++ .zsh/zplug.zsh
@@ -4,12 +4,14 @@ zplug "robbyrussell/oh-my-zsh", use:"lib/*.zsh"
 
 zplug "~/.zsh", from:local, use:"*_*.zsh"
 
-zplug "junegunn/fzf-bin", \
-    from:gh-r, \
-    as:command, \
-    rename-to:fzf, \
-    use:"*linux*amd64*"
+zplug "b4b4r07/emoji-cli"
+zplug "GoogleContainerTools/skaffold", as:command, from:gh-r, rename-to:skaffold
+zplug "junegunn/fzf-bin", from:gh-r, as:command, rename-to:fzf
 zplug "junegunn/fzf", use:"shell/*.zsh", defer:2
+zplug "motemen/ghq", as:command, from:gh-r, rename-to:ghq
+zplug "motemen/ghq", use:"zsh"
+zplug "mrowa44/emojify", as:command
+zplug "stedolan/jq", as:command, from:gh-r, rename-to:jq
 
 zplug "plugins/capistrano", from:oh-my-zsh
 zplug "plugins/colorize", from:oh-my-zsh
@@ -24,7 +26,6 @@ zplug "plugins/history", from:oh-my-zsh
 zplug "plugins/sudo", from:oh-my-zsh
 
 zplug "caarlos0/git-add-remote"
-zplug "motemen/ghq", use:"zsh"
 zplug "paulirish/git-open"
 zplug "wfxr/forgit"
 zplug "zsh-users/zsh-autosuggestions"
