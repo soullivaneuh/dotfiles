df0ad61853c78dcd4ad1bcf40d74c284480470aa
Sullivan SENECHAL <soullivaneuh@gmail.com>
2019-02-22 12:20:39 +0100

    Add pwd shortcut

--- .zsh/30_aliases.zsh
+++ .zsh/30_aliases.zsh
@@ -1,6 +1,7 @@
 # Just typing the file path without command will open them with vim
 alias -s {yml,yaml}=vim
 
+alias p="pwd"
 alias zshreload="source $HOME/.zshrc"
 
 alias -g G='| grep -i' # ls G file_to_match
