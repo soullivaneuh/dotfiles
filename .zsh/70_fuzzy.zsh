580b6d7b546a215fbd3fb72c14d44b61a29ca10d
Sullivan SENECHAL <soullivaneuh@gmail.com>
2019-02-27 12:05:16 +0100

    Improve directories fuzzy matching

--- .zsh/70_fuzzy.zsh
+++ .zsh/70_fuzzy.zsh
@@ -1,13 +1,18 @@
 # ALT-C - Fuzzy commands
 fzf-project-widget() {
-  local cmd="(find -L . -mindepth 1 ; ghq list) 2> /dev/null"
+  local cmd="(find -L . -mindepth 1 -type d ; ghq list) 2> /dev/null"
   setopt localoptions pipefail 2> /dev/null
   local dir="$(eval "$cmd" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS $FZF_ALT_C_OPTS" $(__fzfcmd) +m)"
+
   if [[ -z "$dir" ]]; then
     zle redisplay
     return 0
   fi
-  cd "${HOME}/p/$dir"
+
+  local project_dir="${HOME}/p/$dir"
+  [[ -d "${project_dir}" ]] && dir=${project_dir}
+
+  cd ${dir}
   local ret=$?
   zle fzf-redraw-prompt
   return $ret
