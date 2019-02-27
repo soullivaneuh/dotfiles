72fac6f529792023cbb88c48c944df9742134a24
Sullivan SENECHAL <soullivaneuh@gmail.com>
2019-02-27 12:43:01 +0100

    Fuzzy execute if possible

--- .zsh/70_fuzzy.zsh
+++ .zsh/70_fuzzy.zsh
@@ -1,21 +1,28 @@
 # ALT-C - Fuzzy commands
 fzf-project-widget() {
-  local cmd="(find -L . -mindepth 1 -type d ; ghq list) 2> /dev/null"
+  local cmd="(find -L . -mindepth 1 ; ghq list) 2> /dev/null"
   setopt localoptions pipefail 2> /dev/null
-  local dir="$(eval "$cmd" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS $FZF_ALT_C_OPTS" $(__fzfcmd) +m)"
+  local line="$(eval "$cmd" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse $FZF_DEFAULT_OPTS $FZF_ALT_C_OPTS" $(__fzfcmd) +m)"
 
-  if [[ -z "$dir" ]]; then
+  if [[ -z "${line}" ]]; then
     zle redisplay
     return 0
   fi
 
-  local project_dir="${HOME}/p/$dir"
-  [[ -d "${project_dir}" ]] && dir=${project_dir}
+  local project_dir="${HOME}/p/$line"
+  [[ -d "${project_dir}" ]] && line=${project_dir}
 
-  cd ${dir}
+  if [[ -d "${line}" ]]; then
+    cd ${line}
+  elif [[ -x "${line}" ]]; then
+    ${line}
+  else
+    echo "Nothing to do for ${line}" && false
+  fi
+
   local ret=$?
   zle fzf-redraw-prompt
-  return $ret
+  return ${ret}
 }
 zle     -N    fzf-project-widget
 bindkey '\ec' fzf-project-widget
