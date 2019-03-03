a06c3781d80a5a950c52b817031189ed461ab319
Sullivan SENECHAL <soullivaneuh@gmail.com>
2019-03-03 23:43:18 +0100

    Remove trailling blank line

--- .zsh/00_init.zsh
+++ .zsh/00_init.zsh
@@ -5,4 +5,3 @@ for conf in /etc/subuid /etc/subgid; do
   grep --quiet --line-regexp --fixed-strings -- ${SUBID} ${conf} \
     || sudo sed -i -n -e '/^sullivan:/!p' -e "\$a${SUBID}" ${conf}
 done
-
