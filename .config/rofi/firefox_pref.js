2f6b1aa89299e13549650325229f456402f9ad34
Sullivan SENECHAL <soullivaneuh@gmail.com>
2019-09-03 11:41:48 +0200

    Simplify Firefox project management

--- .config/rofi/firefox_pref.js
+++
@@ -1,4 +0,0 @@
-// Always restore session.
-user_pref("browser.startup.page", 3);
-// I know my rights, thanks.
-user_pref("browser.rights.3.shown", true);
--- .config/rofi/project.sh
+++ .config/rofi/project.sh
@@ -16,35 +16,23 @@ if [[ -z "${selection}" ]]; then
 fi
 PROJECT_PATH=${HOME}/p/${selection}
 
+set +e
+wexists=$(i3-msg -t get_workspaces | jq --raw-output '.[].name' | grep ${selection})
+set -e
 i3-msg workspace ${selection}
+if [[ ! -z "${wexists}" ]]; then
+  exit 0
+fi
+
 if [[ ! -d "${PROJECT_PATH}" ]]; then
   termite --exec="ghq get -p ${selection}"
 fi
 
 # Visual code launch
 code ${PROJECT_PATH}
-
-# Firefox profile setup
-FIREFOX_PROFILE=${HOME}/.firefox-projects/${selection}
-DEFAULT_PROFILE=${HOME}/.mozilla/firefox/$(grep 'Default=' ${HOME}/.mozilla/firefox/profiles.ini | head -n 1 | cut -d = -f2)
-mkdir --parent ${FIREFOX_PROFILE}
-ln --force --symbolic ${DEFAULT_PROFILE}/signedInUser.json ${FIREFOX_PROFILE}
-ln --force --symbolic ${DEFAULT_PROFILE}/logins.json ${FIREFOX_PROFILE}
-ln --force --symbolic ${DEFAULT_PROFILE}/key4.db ${FIREFOX_PROFILE}
-set +e
-cp ${DEFAULT_PROFILE}/cookies.sqlite ${FIREFOX_PROFILE}
-cp ${DEFAULT_PROFILE}/cookies.sqlite-wal ${FIREFOX_PROFILE}
-set -e
-cp ~/.config/rofi/firefox_pref.js ${FIREFOX_PROFILE}/user.js
+sleep 0.5
 
 # Frifox session launch
-PROJECT_URL=http://${selection}
-FIREFOX_PID=$(ps ax | grep ${FIREFOX_PROFILE} | grep -v grep | awk {'print $1'})
-if [[ -z "${FIREFOX_PID}" ]]; then
-  firefox-developer-edition \
-    --profile ${FIREFOX_PROFILE} \
-    ${PROJECT_URL} &
-  echo $! > ${FIREFOX_PID}
-fi
+firefox-developer-edition --new-window http://${selection} &
 
 exit 0
