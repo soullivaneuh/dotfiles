0b1abe2cf82495114a3d34705403bdd4917ac998
Sullivan SENECHAL <soullivaneuh@gmail.com>
2024-01-04 13:33:09 +0100

    feat: podman

--- setup.sh
+++ setup.sh
@@ -132,6 +132,28 @@ sudo apt install docker-ce
 
 sudo usermod --append --groups docker "$(whoami)"
 
+# Podman
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.opensuse.org/repositories/devel:kubic:libcontainers:unstable/Debian_Testing/Release.key \
+  | gpg --dearmor \
+  | sudo tee /etc/apt/keyrings/devel_kubic_libcontainers_unstable.gpg > /dev/null
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/devel_kubic_libcontainers_unstable.gpg]\
+    https://download.opensuse.org/repositories/devel:kubic:libcontainers:unstable/Debian_Testing/ /" \
+  | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:unstable.list > /dev/null
+curl -fsSL https://download.opensuse.org/repositories/devel:kubic:libcontainers:unstable/Debian_Unstable/Release.key \
+  | gpg --dearmor \
+  | sudo tee /etc/apt/keyrings/devel_kubic_libcontainers_unstable.gpg > /dev/null
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/devel_kubic_libcontainers_unstable.gpg]\
+    https://download.opensuse.org/repositories/devel:kubic:libcontainers:unstable/Debian_Unstable/ /" \
+  | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:unstable.list > /dev/null
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y install podman
+# Enable rootls podman socket
+systemctl --user enable --now podman.socket
+
 git submodule init
 git submodule update
 git remote set-url origin git@github.com:soullivaneuh/dotfiles.git
